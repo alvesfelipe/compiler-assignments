d5c6048ac2ca6bc0de5edcf30bf9cff564cdc720
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-02-20 17:45:32 -0300

    Implements recognition of some symbols of alphabet

--- AT-04/automaton-accepter.py
+++ AT-04/automaton-accepter.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 #Deterministic finite accepter
 
-#symbols
-symbols = ['+', '-', '/', '*']
+#some symbols of alphabet
+alphabet_symbols = ['+', '-', '/', '*']
 
 def check_symbols(str_input):
-	if any(element == str_input for element in symbols):
-		print str_input
-		return;
+	for element_str in str_input:
+		if not any(element_str == element_symb for element_symb in alphabet_symbols):
+			print "Error, invalid string: ", str_input
+			return
+	print "String ACCEPTED!"
 
 str = raw_input("Insert the String: ")
-check_symbols(str)
+list_str = list(str)
+
+# print "list string: ", list_str[0:len(list_str)]
+check_symbols(list_str)
